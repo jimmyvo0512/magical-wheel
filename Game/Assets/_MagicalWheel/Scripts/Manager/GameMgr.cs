bed9b572aded2403f6903a92e125acd1ff2f91d2
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-04-05 00:25:50 +0700

    clean handle functions

--- Game/Assets/_MagicalWheel/Scripts/Manager/GameMgr.cs
+++ Game/Assets/_MagicalWheel/Scripts/Manager/GameMgr.cs
@@ -12,64 +12,31 @@ public class GameMgr : Singleton<GameMgr>
         TCPSender.Register(this.playerName);
     }
 
-    public void Connect(bool ok)
+    public void HandleConnecting(bool ok)
     {
-        var status = ok ? "Connected to server!" : "Can't find server!";
-        FindObjectOfType<RegisterSceneMgr>().SetStatus(status, ok);
     }
 
     public void HandleRegisterResp(RegisterResp resp)
     {
-        switch (resp)
-        {
-            case RegisterResp.OK:
-            case RegisterResp.Invalid:
-            case RegisterResp.AlreadyExist:
-            default:
-                break;
-        }
     }
 
     public void HandleNewPlayerInform(string playerName)
     {
-        var status = "Player " + playerName + " just joined!";
-        FindObjectOfType<RegisterSceneMgr>().SetStatus(status, false);
     }
 
     public void HandleStartGame(string question, int answerLen, string playerName)
     {
-        if (SceneManager.GetActiveScene().name != "Game")
-        {
-            SceneManager.LoadSceneAsync("Game").completed += async asyncOpr =>
-            {
-                await Task.Delay(500);
-                FindObjectOfType<GameSceneMgr>().SetQuestion(question, answerLen);
-            };
-
-            return;
-        }
-
-        FindObjectOfType<GameSceneMgr>().SetQuestion(question, answerLen);
     }
 
     public void HandlePlayerTurn(int turn, string playerName)
     {
-        FindObjectOfType<GameSceneMgr>().SetTurn(playerName == this.playerName);
     }
 
     public void HandleCorrectChar(Dictionary<string, int> scoreBoard, string nextPlayerName)
     {
-
     }
 
     public void HandleEndGame(string resultKeyword, Dictionary<string, int> scoreBoard)
     {
-        if (SceneManager.GetActiveScene().name != "Rank")
-        {
-            SceneManager.LoadSceneAsync("Rank").completed += async asyncOpr =>
-            {
-                await Task.Delay(500);
-            };
-        }
     }
 }
